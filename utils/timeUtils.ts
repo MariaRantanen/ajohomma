faa636873cf5f29dfa39fcfd75d98c2960e12e83
MariaRantanen <48141@edu.raseko.fi>
2023-09-11 15:03:44 +0300

    väärä nimi

--- utils/timeUtils.ts
+++
@@ -1,6 +0,0 @@
-export function getRoundedTime(precisionSeconds: number = 1): Date {
-    const milliseconds = new Date().getTime();
-    const precisionMs = 1000 * precisionSeconds;
-    const rounded = Math.floor(milliseconds / precisionMs) * precisionMs;
-    return new Date(rounded);
-}
